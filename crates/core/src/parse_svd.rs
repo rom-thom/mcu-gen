b85f7fdbdb5c507d98f086e00bead76ff5c801a9
Thomas <thomapau@stud.ntnu.no>
2025-07-16 18:34:16 +0200

    adding svd parsing

---
+++ crates/core/src/parse_svd.rs
@@ -0,0 +1,23 @@
+use std::fs;
+
+use svd_parser::{parse, svd::Device};
+
+pub fn parse_svd(path: &str)->Device{
+    let svd_xml = fs::read_to_string(path).expect("couldn't read svd file when trying to parse it");
+    parse(&svd_xml).expect("Couldn't parse the svd file")
+}
+
+
+
+
+
+#[test]
+fn test_parsing(){
+    let device = parse_svd("../../svd/STM32F103.svd");
+    // Print device name
+    println!("Parsed device: {}", device.name);
+
+    let index = 35;
+    dbg!(&device.peripherals[index].name);
+    dbg!(&device.peripherals[index].description);
+}
